0629c124ebd80d3a8f25a9675ed020828acc9355
hjm31ok <58432949+hjm31ok@users.noreply.github.com>
2025-11-10 16:44:16 +0800

    base.sol
    代码置于remix，用于完成base公会任务Base Learn Newcomer

---
+++ base.sol
@@ -0,0 +1,33 @@
+//Hold at least 1 Basic Contracts Pin NFTs(以下代码置于remix，用于完成base公会任务Base Learn Newcomer)
+
+··············································
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract BasicMath {
+    function adder(uint _a, uint _b) public pure returns (uint sum, bool error) {
+        // Use `unchecked` to allow overflow
+        unchecked {
+            uint c = _a + _b;
+            // If the result of the addition is smaller than _a, it means an overflow has occurred
+            if (c < _a) {
+                return (0, true);
+            }
+            return (c, false);
+        }
+    }
+
+    function subtractor(uint _a, uint _b) public pure returns (uint difference, bool error) {
+        // Manually check whether underflow will occur (when _a is smaller than _b)
+        if (_a < _b) {
+            return (0, true);
+        }
+
+        // Use `unchecked` to avoid Solidity's underflow check which will stop the transaction
+        unchecked {
+            uint c = _a - _b;
+            return (c, false);
+        }
+    }
+}
