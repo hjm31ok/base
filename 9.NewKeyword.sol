d68ac673506258fb78ec0a6d454140298437c4ba
hjm31ok <58432949+hjm31ok@users.noreply.github.com>
2025-11-11 11:13:38 +0800

    Hold at least 1 New Keyword Pin NFTs
    Add initial comments for New Keyword NFT task verification

---
+++ 9.NewKeyword.sol
@@ -0,0 +1,9 @@
+//此代码用于验证“Hold at least 1 New Keyword Pin NFTs”任务
+//在remix中Deploy & run transactions选项下
+//Contract选项中选择"AddressBookFactory-你自己的文件名.sol"
+//点击部署合约
+//将合约地址复制到下方链接验证
+//https://docs.base.org/learn/new-keyword/new-keyword-exercise
+
+
+```````````````````````````````````````````````
